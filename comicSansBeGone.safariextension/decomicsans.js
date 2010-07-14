b31a3f6d3e33fcc18ed6f241692dead3d854c57b
Phillip Hutchings <sitharus@sitharus.com>
2010-07-14 17:45:20 +1200

    Update to work on inline styles and FONT tags (yuck), thanks to http://github.com/goblindegook/Allvetica/ and goblindegook for pointing that out.

--- comicSansBeGone.safariextension/decomicsans.js
+++ comicSansBeGone.safariextension/decomicsans.js
@@ -1,13 +1,31 @@
 var stylesheets = document.styleSheets
+var cs = /comic sans ms/i
+var replaceComicSans = function(el) {
+    if (cs.test(el.style.fontFamily)) {
+        el.style.fontFamily = el.style.fontFamily.replace(cs, 'Helvetica')
+    }
+    if (cs.test(el.style.font)) {
+        el.style.font = el.style.font.replace(cs, "Helvetica")
+    }
+}
+
 for(var i =0; i < stylesheets.length; i++) {
     var rules = stylesheets[i].cssRules
     for (var j = 0; j < rules.length; j++) {
-        var rule = rules[j]
-        if (/comic sans ms/i.test(rule.style.fontFamily)) {
-            rule.style.fontFamily = 'Helvetica'
-        }
-        if (/comic sans ms/i.test(rule.style.font)) {
-            rule.style.font = rule.style.font.replace(/comic sans ms/i, "Helvetica")
-        }
+        replaceComicSans(rules[j])
+    }
+}
+
+var styledElements = document.querySelectorAll("*[style*='font']")
+for (var i = 0; i < styledElements.length; i++) {
+    var s = styledElements[i]
+    replaceComicSans(s)
+}
+
+var fontTags = document.getElementsByTagName("font");
+for (var i = 0; i < fontTags.length; i++) {
+    var f = fontTags[i]
+    if (cs.test(f.getAttribute("face"))) {
+        f.setAttribute("face", f.getAttribute("face").replace(cs, "Helvetica"))
     }
 }

521da42819d48c3e2387d7fd0d28341be28a9d4b
sitharus@sitharus.com <Thea>
2010-09-08 21:26:33 +1200

    Add ability to replace Arial as well as Comic Sans MS. Also allow users to choose their own replacement font.

--- comicSansBeGone.safariextension/decomicsans.js
+++ comicSansBeGone.safariextension/decomicsans.js
@@ -1,33 +1,44 @@
-var stylesheets = document.styleSheets
-var cs = /comic sans ms/i
-var replaceComicSans = function(el) {
-    if (el && el.style) {
-        if (cs.test(el.style.fontFamily)) {
-            el.style.fontFamily = el.style.fontFamily.replace(cs, 'Helvetica')
+var handleMessage = function(event) {
+    var stylesheets = document.styleSheets
+    var replace = []
+    if (event.message.replaceComicSans) replace.push('comic sans ms')
+    if (event.message.replaceArial) replace.push('arial')
+    var r = new RegExp("(" + replace.join("|")+")", "i")
+    
+    var replaceFonts = function(el) {
+        if (el && el.style) {
+            if (r.test(el.style.fontFamily)) {
+                el.style.fontFamily = el.style.fontFamily.replace(r, event.message.replaceWith)
+            }
+            if (r.test(el.style.font)) {
+                el.style.font = el.style.font.replace(r, event.message.replaceWith)
+            }
         }
-        if (cs.test(el.style.font)) {
-            el.style.font = el.style.font.replace(cs, "Helvetica")
+    }
+
+    for(var i =0; i < stylesheets.length; i++) {
+        var rules = stylesheets[i].cssRules
+        if (rules) {
+            for (var j = 0; j < rules.length; j++) {
+                replaceFonts(rules[j])
+            }
+        }
+    }
+
+    var styledElements = document.querySelectorAll("*[style*='font']")
+    for (var i = 0; i < styledElements.length; i++) {
+        var s = styledElements[i]
+        replaceFonts(s)
+    }
+
+    var fontTags = document.getElementsByTagName("font");
+    for (var i = 0; i < fontTags.length; i++) {
+        var f = fontTags[i]
+        if (r.test(f.getAttribute("face"))) {
+            f.setAttribute("face", f.getAttribute("face").replace(r, event.message.replaceWith))
         }
     }
 }
 
-for(var i =0; i < stylesheets.length; i++) {
-    var rules = stylesheets[i].cssRules
-    for (var j = 0; j < rules.length; j++) {
-        replaceComicSans(rules[j])
-    }
-}
-
-var styledElements = document.querySelectorAll("*[style*='font']")
-for (var i = 0; i < styledElements.length; i++) {
-    var s = styledElements[i]
-    replaceComicSans(s)
-}
-
-var fontTags = document.getElementsByTagName("font");
-for (var i = 0; i < fontTags.length; i++) {
-    var f = fontTags[i]
-    if (cs.test(f.getAttribute("face"))) {
-        f.setAttribute("face", f.getAttribute("face").replace(cs, "Helvetica"))
-    }
-}
+safari.self.addEventListener("message", handleMessage, false);
+safari.self.tab.dispatchMessage("load-settings", "")
